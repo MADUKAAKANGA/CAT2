c6e9a241028e6a2d3c4749e1a08d77af757640a8
MADUKAAKANGA <138138019+MADUKAAKANGA@users.noreply.github.com>
2023-11-17 16:23:19 +0300

    Add files via upload
    create editable table

---
+++ ViewAuthors.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <a href="indexx.php">Home</a>
+    <a href="ViewAuthors.php">View Authors</a>
+</head>
+
+<body>
+    <?php
+    include 'configs/DbConn.php';
+$stmt = $pdo->query("SELECT * FROM authorstb");
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Display the data in an editable table
+    echo '<form method="post">';
+    echo '<table>';
+    echo '<tr><th>Author ID</th><th>Full Name</th><th>Email</th></tr>';
+    foreach ($data as $row) {
+        echo '<tr>';
+        echo '<td>' . $row['author_id'] . '</td>';
+        echo '<td><input type="text" name="data['.$row['author_id'].'][fullname]" value="' . $row['fullname'] . '"></td>';
+        echo '<td><input type="text" name="data['.$row['author_id'].'][email]" value="' . $row['email'] . '"></td>';
+        echo '</tr>';
+        echo '</tr>';
+        
+        echo '</tr>';
+    }
+        ?>
+</body>
+</html>
